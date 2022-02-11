b983295574b29c1d7b6ef895665c1ab9d1bc0c90
Eduard <dawbevidalgallardo@iesjoanramis.org>
2022-02-11 17:20:53 +0100

    a

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4>Indica un número enter: </h4>
+    <input type="number" v-model="n"><button v-on:click="afegirNum()">Entrar</button>
+    <h1>Llista de números</h1>
+    <Number v-on:mostrarNum="mostrarNum" v-for="a in nArray" v-bind:num="a" v-bind:key="a"/>
+    <h4>El número pitjat és: <span id="numb"></span></h4>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Number from './components/Number.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Number
+  },
+  data() {
+    return {
+      nArray: []
+    }
+  },
+  methods: {
+      afegirNum : function() {
+        var esRepe = false;
+          for (var i = 0; i < this.nArray.length; i++) {
+            if (this.nArray[i]==this.n) {
+              esRepe = true;
+            }
+          }
+          if (!esRepe) {
+            this.nArray.push(this.n);
+          } else { alert("Nombre repetit, afegeix-ne un altre"); }
+          
+      },
+      mostrarNum : function(num) {
+        var txt = document.getElementById("numb");
+        txt.innerHTML = num;
+      }
+      
   }
 }
 </script>
